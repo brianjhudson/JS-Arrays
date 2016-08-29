abb619c3ba1c93cd4f99400e5b97df7317cf99e6
Brian Hudson <brianjasonhudson@gmail.com>
2016-08-29 14:41:28 -0500

    Added evenFinder function

--- arraysPractice.js
+++ arraysPractice.js
@@ -56,7 +56,14 @@ var nums = [1,2,3,6,22,98,45,23,22,12];
 //Write a function named evenFinder that is given nums as it's only argument and removes all values that aren't even from the given array.
 
   //Code Here
-
+function evenFinder(nums) {
+  for (var i = 0; i < nums.length; i++) {
+    if (nums[i] % 2 !== 0) {
+      nums.splice(i, 1);
+    }
+  }
+  return nums;
+}
 
 //Next problem
 
