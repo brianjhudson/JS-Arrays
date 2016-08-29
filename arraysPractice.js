0ff2029f5fa692e1ad6b23b6798150403a5e4208
Brian Hudson <brianjasonhudson@gmail.com>
2016-08-29 15:42:47 -0500

    Added both function to find common elements

--- arraysPractice.js
+++ arraysPractice.js
@@ -246,7 +246,17 @@ function longer(arr1, arr2) {
 */
 
   //Code Here
-
+function both(arr1, arr2) {
+  var common = [];
+  for (var i = 0; i < arr1.length; i++) {
+    for (var j = 0; j < arr2.length; j++) {
+      if (arr1[i] === arr2[j]) {
+        common.push(arr1[i]);
+      }
+    }
+  }
+  return common;
+}
 
 
 
