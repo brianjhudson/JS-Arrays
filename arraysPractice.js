dec29d1466ea319c946369fa124a4ba4bcf51046
Brian Hudson <brianjasonhudson@gmail.com>
2016-08-29 14:37:16 -0500

    Added looper function

--- arraysPractice.js
+++ arraysPractice.js
@@ -15,12 +15,12 @@ function first(arr) {
 
 var arr = [40,50,60];
 //Create a function named 'last' that is given 'arr' as the argument and returns the last item in the given array.
-function last(arr) {
-  return arr[arr.length - 1];
-}
 
   //Code Here
 
+  function last(arr) {
+    return arr[arr.length - 1];
+  }
 
 //Next Problem
 
@@ -29,7 +29,11 @@ var family = ['Tyler', 'Jordyn', 'Ryan', 'Chelsey', 'Ireland'];
 //Create a function named 'looper' that is given family as it's only argument, loops through the given array, and alerts every item in the array.
 
   //Code Here
-
+function looper(family) {
+  for (var i = 0; i < family.length; i++) {
+    alert(family[i]);
+  }
+}
 
 //Next problem
 
